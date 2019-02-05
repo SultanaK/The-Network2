c8724de866567732a93805279a17b9cea5f46122
Sultana <kr.shampa@yahoo.com>
2019-02-05 14:22:52 -0600

    fix footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
   return (
-    <footer className="bg-primary text-white mt-5 p-4 text-center">
+    <footer className="card-footer bg-transparent text-black mt-5 p-4 text-center">
       Copyright &copy; {new Date().getFullYear()} The Network
     </footer>
   );
