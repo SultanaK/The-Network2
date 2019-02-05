a29f2ebee32db86162467d0d68b744e24934c941
Sultana <kr.shampa@yahoo.com>
2019-02-05 04:53:46 -0600

    deploy Heroku

--- server.js
+++ server.js
@@ -38,8 +38,8 @@ app.use("/api/posts", posts);
 if (process.env.NODE_ENV === 'production') {
     //set static folder
     app.use(express.static('client/build'));
-    //app.get('*', (req,res) => {
-      app.get(/^\/(?!api).*/, (req, res) => {
+    app.get('*', (req,res) => {
+    //  app.get(/^\/(?!api).*/, (req, res) => {
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
